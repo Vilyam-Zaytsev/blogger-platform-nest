cf6f2b36c63f72162327ae14268f8656e37dfbb9
Vilyam <z.v.m.365@gmail.com>
2025-04-24 14:58:47 +0300

    fix user.entity.spec.ts

--- src/modules/user-accounts/domain/user.entity.spec.ts
+++ src/modules/user-accounts/domain/user.entity.spec.ts
@@ -1,9 +1,13 @@
 import { CreateUserDomainDto } from './dto/create-user.domain.dto';
 import { User } from './user.entity';
-import { ConfirmationStatus } from './email-confirmation.schema';
+import {
+  ConfirmationStatus,
+  EmailConfirmation,
+} from './email-confirmation.schema';
+import { PasswordRecovery } from './password-recovery.schema';
 
 describe('User Entity - createInstance()', () => {
-  it('should correctly create a User instance with nested entities(ConfirmationStatus: "NotConfirmed")', () => {
+  it('should correctly create a User instance with nested entities(ConfirmationStatus: "Confirmed")', () => {
     const dto: CreateUserDomainDto = {
       login: 'test_user',
       email: 'test_user@example.com',
@@ -11,24 +15,41 @@ describe('User Entity - createInstance()', () => {
       isConfirmed: true,
     };
 
-    //TODO: как правильно тестировать!?
     const user = User.createInstance(dto);
 
-    console.log(user);
+    expect(user.login).toBe(dto.login);
+    expect(user.email).toBe(dto.email);
+    expect(user.passwordHash).toBe(dto.passwordHash);
 
-    expect(user).toEqual({
-      login: dto.login,
-      email: dto.email,
-      passwordHash: dto.passwordHash,
-      passwordRecovery: {
-        recoveryCode: null,
-        expirationDate: null,
-      },
-      emailConfirmation: {
-        confirmationCode: null,
-        expirationDate: null,
-        confirmationStatus: ConfirmationStatus.Confirmed,
-      },
-    });
+    expect(user).toHaveProperty('passwordRecovery');
+    expect(user.passwordRecovery).toBeInstanceOf(PasswordRecovery);
+
+    expect(user.emailConfirmation).toBeInstanceOf(EmailConfirmation);
+    expect(user.emailConfirmation.confirmationStatus).toBe(
+      ConfirmationStatus.Confirmed,
+    );
+  });
+
+  it('should correctly create a User instance with nested entities(ConfirmationStatus: "NotConfirmed")', () => {
+    const dto: CreateUserDomainDto = {
+      login: 'test_user',
+      email: 'test_user@example.com',
+      passwordHash: 'hashedPassword',
+      isConfirmed: false,
+    };
+
+    const user = User.createInstance(dto);
+
+    expect(user.login).toBe(dto.login);
+    expect(user.email).toBe(dto.email);
+    expect(user.passwordHash).toBe(dto.passwordHash);
+
+    expect(user).toHaveProperty('passwordRecovery');
+    expect(user.passwordRecovery).toBeInstanceOf(PasswordRecovery);
+
+    expect(user.emailConfirmation).toBeInstanceOf(EmailConfirmation);
+    expect(user.emailConfirmation.confirmationStatus).toBe(
+      ConfirmationStatus.NotConfirmed,
+    );
   });
 });
