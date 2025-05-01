f668368dea13e3b343d6dfd28e65002815cf3cf6
Vilyam <z.v.m.365@gmail.com>
2025-05-01 20:22:11 +0300

    test(blogs): add unit test for updateBlog method

--- src/modules/bloggers-platform/domain/blog.entity.spec.ts
+++ src/modules/bloggers-platform/domain/blog.entity.spec.ts
@@ -1,10 +1,10 @@
-import { CreateBlogDomainDto } from './dto/create-blog.domain.dto';
+import { CreateBlogDto, UpdateBlogDto } from './dto/createBlogDto';
 import { Blog, BlogDocument } from './blog.entity';
 import { TestLoggers } from '../../../../test/helpers/test-loggers';
 
 describe('Blog Entity - createInstance()', () => {
   it('should correctly create a Blog instance.', () => {
-    const dto: CreateBlogDomainDto = {
+    const dto: CreateBlogDto = {
       name: 'test_blog',
       description: 'test_description',
       websiteUrl: 'www.test_website_url.com',
@@ -27,7 +27,7 @@ describe('Blog Entity - createInstance()', () => {
   });
 
   it('should set deletedAt to the current date, if not already deleted using the "makeDeleted" method.', () => {
-    const dto: CreateBlogDomainDto = {
+    const dto: CreateBlogDto = {
       name: 'test_blog',
       description: 'test_description',
       websiteUrl: 'www.test_website_url.com',
@@ -47,8 +47,8 @@ describe('Blog Entity - createInstance()', () => {
     );
   });
 
-  it('should throw an error if the user has already been marked as deleted.', () => {
-    const dto: CreateBlogDomainDto = {
+  it('should throw an error if the blog has already been marked as deleted.', () => {
+    const dto: CreateBlogDto = {
       name: 'test_blog',
       description: 'test_description',
       websiteUrl: 'www.test_website_url.com',
@@ -62,7 +62,35 @@ describe('Blog Entity - createInstance()', () => {
 
     TestLoggers.logUnit<BlogDocument>(
       blog,
-      'Test №3: User Entity - makeDeleted()',
+      'Test №3: Blog Entity - makeDeleted()',
     );
+  });
+
+  it('should correctly update blog properties when provided with valid data.', () => {
+    const createDto: CreateBlogDto = {
+      name: 'test_blog',
+      description: 'test_description',
+      websiteUrl: 'www.test_website_url.com',
+    };
+
+    const updateDto: UpdateBlogDto = {
+      name: 'update_test_blog',
+      description: 'update_test_description',
+      websiteUrl: 'www.update_test_website_url.com',
+    };
+
+    const blog: BlogDocument = Blog.createInstance(createDto);
+
+    expect(blog.name).toBe(createDto.name);
+    expect(blog.description).toBe(createDto.description);
+    expect(blog.websiteUrl).toBe(createDto.websiteUrl);
+
+    blog.update(updateDto);
+
+    expect(blog.name).toBe(updateDto.name);
+    expect(blog.description).toBe(updateDto.description);
+    expect(blog.websiteUrl).toBe(updateDto.websiteUrl);
+
+    TestLoggers.logUnit<BlogDocument>(blog, 'Test №4: Blog Entity - update()');
   });
 });
