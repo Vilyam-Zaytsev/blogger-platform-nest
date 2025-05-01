c2cd715704027a26a741232ba2d3ee45bc832e63
Vilyam <z.v.m.365@gmail.com>
2025-05-01 23:43:47 +0300

    feat(posts): start implementing PostsController

--- src/modules/bloggers-platform/posts/api/posts.controller.ts
+++ src/modules/bloggers-platform/posts/api/posts.controller.ts
@@ -1,41 +1,42 @@
-// import { Body, Controller, Post } from '@nestjs/common';
-// import { BlogViewDto } from '../../blogs/api/view-dto/blog-view.dto';
-// import { PostInputDto } from './input-dto/post-input.dto';
-//
-// @Controller('posts')
-// export class PostsController {
-//   constructor() {} // private readonly blogsQueryRepository: BlogsQueryRepository, // private readonly deleteBlogUseCase: DeleteBlogUseCase, // private readonly updateBlogUseCase: UpdateBlogUseCase, // private readonly createBlogUseCase: CreateBlogUseCase,
-//   // @Get()
-//   // async getAll(
-//   //   @Query() query: GetBlogsQueryParams,
-//   // ): Promise<PaginatedViewDto<BlogViewDto>> {
-//   //   return this.blogsQueryRepository.getAll(query);
-//   // }
-//
-//   // @Get(':id')
-//   // async getById(@Param('id') id: string): Promise<BlogViewDto> {
-//   //   return this.blogsQueryRepository.getByIdOrNotFoundFail(id);
-//   // }
-//
-//   @Post()
-//   async createPost(@Body() body: PostInputDto): Promise<PostViewDto> {
-//     const blogId: string = await this.createBlogUseCase.execute(body);
-//
-//     return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
-//   }
-//
-//   // @Put(':id')
-//   // @HttpCode(HttpStatus.NO_CONTENT)
-//   // async updateBlog(
-//   //   @Param('id') id: string,
-//   //   @Body() body: BlogInputDto,
-//   // ): Promise<void> {
-//   //   await this.updateBlogUseCase.execute(id, body);
-//   // }
-//   //
-//   // @Delete(':id')
-//   // @HttpCode(HttpStatus.NO_CONTENT)
-//   // async deleteBlog(@Param('id') id: string): Promise<void> {
-//   //   await this.deleteBlogUseCase.execute(id);
-//   // }
-// }
+import { Body, Controller, Post } from '@nestjs/common';
+import { BlogViewDto } from '../../blogs/api/view-dto/blog-view.dto';
+import { PostInputDto } from './input-dto/post-input.dto';
+import { PostViewDto } from './view-dto/post-view.dto';
+
+@Controller('posts')
+export class PostsController {
+  constructor() {} // private readonly blogsQueryRepository: BlogsQueryRepository, // private readonly deleteBlogUseCase: DeleteBlogUseCase, // private readonly updateBlogUseCase: UpdateBlogUseCase, // private readonly createBlogUseCase: CreateBlogUseCase,
+  // @Get()
+  // async getAll(
+  //   @Query() query: GetBlogsQueryParams,
+  // ): Promise<PaginatedViewDto<BlogViewDto>> {
+  //   return this.blogsQueryRepository.getAll(query);
+  // }
+
+  // @Get(':id')
+  // async getById(@Param('id') id: string): Promise<BlogViewDto> {
+  //   return this.blogsQueryRepository.getByIdOrNotFoundFail(id);
+  // }
+
+  @Post()
+  async createPost(@Body() body: PostInputDto): Promise<PostViewDto> {
+    const postId: string = await this.createBlogUseCase.execute(body);
+
+    return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
+  }
+
+  // @Put(':id')
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // async updateBlog(
+  //   @Param('id') id: string,
+  //   @Body() body: BlogInputDto,
+  // ): Promise<void> {
+  //   await this.updateBlogUseCase.execute(id, body);
+  // }
+  //
+  // @Delete(':id')
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // async deleteBlog(@Param('id') id: string): Promise<void> {
+  //   await this.deleteBlogUseCase.execute(id);
+  // }
+}
--- src/modules/bloggers-platform/posts/api/view-dto/post-view.dto.ts
+++ src/modules/bloggers-platform/posts/api/view-dto/post-view.dto.ts
@@ -1,33 +1,49 @@
-// import { BlogDocument } from '../../domain/blog.entity';
-// import { BlogViewDto } from '../../../blogs/api/view-dto/blog-view.dto';
-//
-// export class PostViewDto {
-//   id: string;
-//   title: string;
-//   shortDescription: string;
-//   content: string;
-//   blogId: string;
-//   blogName: string;
-//   extendedLikesInfo: ExtendedLikesInfo;
-//   createdAt: string;
-//
-//   static mapToView(blog: BlogDocument): BlogViewDto {
-//     const dto = new this();
-//
-//     dto.id = blog._id.toString();
-//     dto.name = blog.name;
-//     dto.description = blog.description;
-//     dto.websiteUrl = blog.websiteUrl;
-//     dto.createdAt = blog.createdAt.toISOString();
-//     dto.isMembership = blog.isMembership;
-//
-//     return dto;
-//   }
-// }
-//
-// type ExtendedLikesInfo = {
-//   likesCount: number;
-//   dislikesCount: number;
-//   myStatus: LikeStatus;
-//   newestLikes: LikeDetailsViewModel[];
-// };
+export class PostViewDto {
+  id: string;
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
+  blogName: string;
+  extendedLikesInfo: ExtendedLikesInfo;
+  createdAt: string;
+
+  static mapToView(post: PostDocument): PostViewDto {
+    const dto = new this();
+
+    dto.id = post._id.toString();
+    dto.title = post.title;
+    dto.shortDescription = post.shortDescription;
+    dto.content = post.content;
+    dto.blogId = post.blogId;
+    dto.blogName = post.blogName;
+    (dto.extendedLikesInfo = {
+      likesCount: 0,
+      dislikesCount: 0,
+      myStatus: LikeStatus.None,
+      newestLikes: [],
+    }),
+      (dto.createdAt = post.createdAt.toISOString());
+
+    return dto;
+  }
+}
+
+type ExtendedLikesInfo = {
+  likesCount: number;
+  dislikesCount: number;
+  myStatus: LikeStatus;
+  newestLikes: LikeDetails[];
+};
+
+type LikeDetails = {
+  addedAt: string;
+  userId: string;
+  login: string;
+};
+
+enum LikeStatus {
+  None = 'None',
+  Like = 'Like',
+  Dislike = 'Dislike',
+}
