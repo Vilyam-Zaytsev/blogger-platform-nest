2fd915c664fc238bf3d29f0b3861ee2553ea5c41
Vilyam <z.v.m.365@gmail.com>
2025-05-01 16:10:42 +0300

    feat(blogs): implement createBlog method in BlogsController

--- src/modules/bloggers-platform/api/blogs.controller.ts
+++ src/modules/bloggers-platform/api/blogs.controller.ts
@@ -1,9 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { BlogsInputDto } from './input-dto/blogs.input-dto';
 import { BlogsViewDto } from './view-dto/blogs.view-dto';
+import { CreateBlogUseCase } from '../application/usecases/create-blog.usecase';
+import { BlogsQueryRepository } from '../infrastructure/query/blogs.query-repository';
 
 @Controller('blogs')
 export class BlogsController {
+  constructor(
+    private readonly createBlogUseCase: CreateBlogUseCase,
+    private readonly blogsQueryRepository: BlogsQueryRepository,
+  ) {}
   @Post()
-  async createBlog(@Body body: BlogsInputDto): Promise<BlogsViewDto> {}
+  async createBlog(@Body() body: BlogsInputDto): Promise<BlogsViewDto> {
+    const blogId: string = await this.createBlogUseCase.execute(body);
+
+    return this.blogsQueryRepository.getByIdOrNotFoundFail(blogId);
+  }
 }
