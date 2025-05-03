f48155a28f855fe0b4163c79b07587cef8a1e501
Vilyam <z.v.m.365@gmail.com>
2025-05-03 17:29:05 +0300

    add TODO

--- src/modules/bloggers-platform/blogs/application/usecases/update-blog.usecase.ts
+++ src/modules/bloggers-platform/blogs/application/usecases/update-blog.usecase.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BlogsRepository } from '../../infrastructure/blogs.repository';
 import { BlogDocument } from '../../domain/blog.entity';
 import { UpdateBlogDto } from '../../dto/blog.dto';
-
+//TODO: нужно ли обновлять blogName в комменте при обновлении блога???
 @Injectable()
 export class UpdateBlogUseCase {
   constructor(private readonly blogsRepository: BlogsRepository) {}
