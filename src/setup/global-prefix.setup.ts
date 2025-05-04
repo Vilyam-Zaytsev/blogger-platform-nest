d1ed5b8a1906d84ac6a67e09d4d5f5e94f47b4f8
Vilyam <z.v.m.365@gmail.com>
2025-05-04 15:51:41 +0300

    chore: add swaggerSetup

--- src/setup/global-prefix.setup.ts
+++ src/setup/global-prefix.setup.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from '@nestjs/common';
 
+export const GLOBAL_PREFIX: string = 'api';
+
 function globalPrefixSetup(app: INestApplication) {
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(GLOBAL_PREFIX);
 }
 
 export { globalPrefixSetup };
