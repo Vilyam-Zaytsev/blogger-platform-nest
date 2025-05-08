b7ad4e1688820d7ed22ba7a16ce8472a1f00d35e
Vilyam <z.v.m.365@gmail.com>
2025-05-08 23:35:54 +0300

    feat(user): add uniqueness check for login and email before user creation

--- src/modules/user-accounts/application/usecases/create-user-by-admin.usecase.ts
+++ src/modules/user-accounts/application/usecases/create-user-by-admin.usecase.ts
@@ -3,6 +3,8 @@ import { UsersRepository } from '../../infrastructure/users.repository';
 import { UserDocument } from '../../domain/user.entity';
 import { CreateUserDto } from '../../dto/create-user.dto';
 import { UsersFactory } from '../users.factory';
+import { DomainException } from '../../../../core/exceptions/damain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
 
 @Injectable()
 export class CreateUserByAdminUseCase {
@@ -12,6 +14,28 @@ export class CreateUserByAdminUseCase {
   ) {}
 
   async execute(dto: CreateUserDto): Promise<string> {
+    const { login, email } = dto;
+
+    const doesLoginExist: UserDocument | null =
+      await this.usersRepository.getByLogin(login);
+
+    if (doesLoginExist) {
+      throw new DomainException({
+        code: DomainExceptionCode.BadRequest,
+        message: 'User with the same login already exists.',
+      });
+    }
+
+    const doesEmailExist: UserDocument | null =
+      await this.usersRepository.getByLogin(email);
+
+    if (doesEmailExist) {
+      throw new DomainException({
+        code: DomainExceptionCode.BadRequest,
+        message: 'User with the same email already exists.',
+      });
+    }
+
     const user: UserDocument = await this.userFactory.create(dto);
 
     user.confirmByAdmin();
