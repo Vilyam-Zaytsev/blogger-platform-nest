de2a5357fbdfa03dd26b817f8ebeeaef07528667
Vilyam <z.v.m.365@gmail.com>
2025-05-08 22:06:32 +0300

    feat(query): add validation rules to BaseQueryParams and GetUsersQueryParams

--- src/core/dto/base.query-params.input-dto.ts
+++ src/core/dto/base.query-params.input-dto.ts
@@ -1,4 +1,5 @@
 import { Type } from 'class-transformer';
+import { IsEnum, IsNumber } from 'class-validator';
 
 export enum SortDirection {
   Ascending = 'asc',
@@ -7,10 +8,16 @@ export enum SortDirection {
 
 export abstract class BaseQueryParams<T> {
   @Type(() => Number)
+  @IsNumber()
   pageNumber: number = 1;
+
+  @IsNumber()
   @Type(() => Number)
   pageSize: number = 10;
+
+  @IsEnum(SortDirection)
   sortDirection: SortDirection = SortDirection.Descending;
+
   abstract sortBy: T;
 
   calculateSkip(): number {
