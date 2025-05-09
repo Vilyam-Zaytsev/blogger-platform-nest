8f77f4d2690aa8bab2854672103941eaa30dc417
Vilyam <z.v.m.365@gmail.com>
2025-05-09 23:25:39 +0300

    refactor(auth): change property initialization method for Reflector and ConfigService in BasicAuthGuard

--- src/modules/user-accounts/guards/basic-auth.guard.ts
+++ src/modules/user-accounts/guards/basic-auth.guard.ts
@@ -5,13 +5,36 @@ import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 import { DomainExceptionCode } from '../../../core/exceptions/domain-exception-codes';
 import { DomainException } from '../../../core/exceptions/damain-exceptions';
 import { parseBasicAuth } from '../../../core/utils/basic-auth.util';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class BasicAuthGuard implements CanActivate {
-  private readonly validUsername: string = 'admin';
-  private readonly validPassword: string = 'qwerty';
+  private readonly validUsername: string;
+  private readonly validPassword: string;
 
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    private configService: ConfigService,
+  ) {
+    //TODO: уточнить на уроке какй метод определения validUsername и validPassword предпочтительнее.
+
+    //вариант 1
+    // this.validUsername = this.configService.get<string>('VALID_USERNAME', 'admin');
+    // this.validPassword = this.configService.get<string>('VALID_PASSWORD', 'qwerty');
+
+    //вариант 2
+    const username = this.configService.get<string>('ADMIN_LOGIN');
+    const password = this.configService.get<string>('ADMIN_PASSWORD');
+    //TODO: уточнить на уроке(как лучше выбросить исключение?).
+    if (!username || !password) {
+      throw new Error(
+        'ADMIN_LOGIN and ADMIN_PASSWORD must be set in environment variables',
+      );
+    }
+
+    this.validUsername = username;
+    this.validPassword = password;
+  }
 
   canActivate(context: ExecutionContext): boolean {
     const isPublic: boolean = this.reflector.getAllAndOverride<boolean>(
