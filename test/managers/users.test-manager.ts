45e9e77d19b7c1466987cdbf93092475e0f248fa
Vilyam <z.v.m.365@gmail.com>
2025-05-11 12:16:20 +0300

    refactor(UsersTestManager): update logic of getAll method

--- test/managers/users.test-manager.ts
+++ test/managers/users.test-manager.ts
@@ -1,16 +1,34 @@
-import { UsersQueryRepository } from '../../src/modules/user-accounts/infrastructure/query/users.query-repository';
 import { PaginatedViewDto } from '../../src/core/dto/paginated.view-dto';
 import { UserViewDto } from '../../src/modules/user-accounts/api/view-dto/user.view-dto';
+import { INestApplication } from '@nestjs/common';
+import request, { Response } from 'supertest';
+import { Server } from 'http';
+import { TestUtils } from '../helpers/test.utils';
+import { ConfigService } from '@nestjs/config';
 import { GetUsersQueryParams } from '../../src/modules/user-accounts/api/input-dto/get-users-query-params.input-dto';
+import { GLOBAL_PREFIX } from '../../src/setup/global-prefix.setup';
 
 export class UsersTestManager {
-  private usersQueryRepository: UsersQueryRepository;
+  constructor(
+    private readonly app: INestApplication,
+    private readonly configService: ConfigService,
+  ) {}
 
-  constructor(usersQueryRepository: UsersQueryRepository) {
-    this.usersQueryRepository = usersQueryRepository;
-  }
-  //TODO: исправить {} as GetUsersQueryParams в getAll
-  async getAllUsers(): Promise<PaginatedViewDto<UserViewDto>> {
-    return await this.usersQueryRepository.getAll({} as GetUsersQueryParams);
+  async getAll(
+    query: Partial<GetUsersQueryParams> = {},
+  ): Promise<PaginatedViewDto<UserViewDto>> {
+    const response: Response = await request(this.app.getHttpServer() as Server)
+      .get(`/${GLOBAL_PREFIX}/users`)
+      .query(query)
+      .set(
+        'Authorization',
+        TestUtils.encodingAdminDataInBase64(
+          this.configService.get('ADMIN_LOGIN')!,
+          this.configService.get('ADMIN_PASSWORD')!,
+        ),
+      )
+      .expect(200);
+
+    return response.body as PaginatedViewDto<UserViewDto>;
   }
 }
