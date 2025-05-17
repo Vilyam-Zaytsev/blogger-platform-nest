b9f2b8bff721a202b4238a5c59b8c0fe7632dba4
Vilyam <z.v.m.365@gmail.com>
2025-05-17 16:13:27 +0300

    feat(notifications): create NotificationsModule

---
+++ src/modules/notifications/notifications.module.ts
@@ -0,0 +1,40 @@
+import { SendConfirmationEmailWhenUserRegisteredEventHandler } from './event-handlers/send-confirmation-email-when-user-registered.event-handler';
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EmailService } from './email.service';
+import { ConfigService } from '@nestjs/config';
+import { configModule } from '../../dynamic-config.module';
+import { EmailTemplates } from './templates/email.templates';
+
+@Module({
+  imports: [
+    configModule,
+    MailerModule.forRootAsync({
+      imports: [configModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const email: string | undefined = configService.get<string>('EMAIL');
+        const password: string | undefined =
+          configService.get<string>('EMAIL_PASSWORD');
+
+        if (!email || !password) {
+          throw new Error(
+            'EMAIL and EMAIL_PASSWORD must be defined in environment variables',
+          );
+        }
+
+        return {
+          transport: `smtps://${encodeURIComponent(email)}:${encodeURIComponent(password)}@smtp.gmail.com`,
+          defaults: { from: `Blogger Platform <${email}>` },
+        };
+      },
+    }),
+  ],
+  providers: [
+    EmailService,
+    EmailTemplates,
+    SendConfirmationEmailWhenUserRegisteredEventHandler,
+  ],
+  exports: [EmailService],
+})
+export class NotificationsModule {}
