fd625ba660764d399419b28e6c9d518d9d208342
Vilyam <z.v.m.365@gmail.com>
2025-05-24 14:25:17 +0300

    chore(tests): update Jest configuration

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,21 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+//  "jest": {
+//    "moduleFileExtensions": [
+//      "js",
+//      "json",
+//      "ts"
+//    ],
+//    "rootDir": "src",
+//    "testRegex": ".*\\.spec\\.ts$",
+//    "transform": {
+//      "^.+\\.(t|j)s$": "ts-jest"
+//    },
+//    "collectCoverageFrom": [
+//      "**/*.(t|j)s"
+//    ],
+//    "coverageDirectory": "../coverage",
+//    "testEnvironment": "node"
+//  }
