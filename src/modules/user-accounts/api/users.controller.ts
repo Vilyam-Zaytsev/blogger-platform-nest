8ccc59e8b12b26ae1b01ef66fa4757abf70ef537
Vilyam <z.v.m.365@gmail.com>
2025-05-28 22:34:41 +0300

    refactor(user): switch from direct use case call to commandBus execution for creating user

--- src/modules/user-accounts/api/users.controller.ts
+++ src/modules/user-accounts/api/users.controller.ts
@@ -12,23 +12,23 @@ import {
 } from '@nestjs/common';
 import { UserInputDto } from './input-dto/user.input-dto';
 import { UsersQueryRepository } from '../infrastructure/query/users.query-repository';
-import { CreateUserByAdminUseCase } from '../application/usecases/create-user-by-admin.usecase';
 import { GetUsersQueryParams } from './input-dto/get-users-query-params.input-dto';
 import { PaginatedViewDto } from '../../../core/dto/paginated.view-dto';
 import { UserViewDto } from './view-dto/user.view-dto';
 import { DeleteUserUseCase } from '../application/usecases/delete-user.usecase';
 import { BasicAuthGuard } from '../guards/basic/basic-auth.guard';
 import { IdInputDto } from './input-dto/id.input-dto';
-import { QueryBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetUsersQuery } from '../application/queries/users/get-users.query-handler';
+import { CreateUserCommand } from '../application/usecases/create-user-by-admin.usecase';
 
 @Controller('users')
 @UseGuards(BasicAuthGuard)
 export class UsersController {
   constructor(
-    private readonly createUserByAdminUseCase: CreateUserByAdminUseCase,
     private readonly deleteUserByAdminUseCase: DeleteUserUseCase,
     private readonly usersQueryRepository: UsersQueryRepository,
+    private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
   ) {}
 
@@ -41,7 +41,9 @@ export class UsersController {
 
   @Post()
   async createUser(@Body() body: UserInputDto): Promise<UserViewDto> {
-    const userId: string = await this.createUserByAdminUseCase.execute(body);
+    const userId: string = await this.commandBus.execute(
+      new CreateUserCommand(body),
+    );
 
     return this.usersQueryRepository.getByIdOrNotFoundFail(userId);
   }
