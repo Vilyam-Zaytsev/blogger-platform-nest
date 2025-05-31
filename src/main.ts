db199287fb50d2d407ac582ab66a5cd400377e93
Vilyam <z.v.m.365@gmail.com>
2025-06-01 00:15:58 +0300

    test CI/CD 2

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   await app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log('xxxxxxxxxxxxxxxxxx');
+    console.log('yyyyyyyyyyyyyyyy');
   });
 }
 
