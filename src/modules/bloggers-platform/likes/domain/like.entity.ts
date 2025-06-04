d3b84e5090acda72220805e9954ea3c9d3af4f31
Vilyam <z.v.m.365@gmail.com>
2025-06-04 13:29:14 +0300

    feat(likes): define Mongoose schema for Like entity

--- src/modules/bloggers-platform/likes/domain/like.entity.ts
+++ src/modules/bloggers-platform/likes/domain/like.entity.ts
@@ -1,4 +1,6 @@
-import { Prop, Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { CreateLikeDto } from '../dto/like.dto';
+import { HydratedDocument, Model } from 'mongoose';
 
 export enum LikeStatus {
   None = 'None',
@@ -28,4 +30,24 @@ export class Like {
   createdAt: Date;
 
   updatedAt: Date;
+
+  static createInstance(dto: CreateLikeDto): LikeDocument {
+    const { status, userId, parentId } = dto;
+
+    const like = new this();
+
+    like.status = status;
+    like.userId = userId;
+    like.parentId = parentId;
+
+    return like as LikeDocument;
+  }
 }
+
+export const LikeSchema = SchemaFactory.createForClass(Like);
+
+LikeSchema.loadClass(Like);
+
+export type LikeDocument = HydratedDocument<Like>;
+
+export type LikeModelType = Model<LikeDocument> & typeof Like;
